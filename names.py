33eb19c97830843d0ec54fb02d9a66909a91f9a9
Akshay Rai <akshaykarunaputtur@gmail.com>
2026-03-26 11:12:42 +0530

    reorganized project folder

--- names.py
+++
@@ -1,4 +0,0 @@
-name = input("enter your name:")
-file = open("names.csv","a")
-file.write(f"{name}\n")
-file.close()
--- names3.py
+++
@@ -1,9 +0,0 @@
-import csv
-name = input("whats your name?")
-home = input("whers your house?")
-
-with open("names.csv","a") as file:
-    writer = csv.DictWriter(file, fieldnames = ["name","home"])
-    writer.writerow({"name":name,"home":home})
-    
-    
